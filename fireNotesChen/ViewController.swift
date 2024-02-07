f28934363c443cedb2c5d6c12c09d7fd934ab1a9
Alvin <thisiseasytoremember1224@gmail.com>
2024-02-07 09:16:09 -0600

    Initial Commit

---
+++ fireNotesChen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fireNotesChen
+//
+//  Created by ALVIN CHEN on 2/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
